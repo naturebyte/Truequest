f091413d00d0c91e6280c13934c5f2ba636df652
M Nabeel <unfolks@gmail.com>
2026-04-07 18:10:47 +0530

    redirects added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/forms",
+        has: [
+          {
+            type: "host",
+            value: "www.truequestlearning.com",
+          },
+        ],
+        destination: "https://forms.truequestlearning.com",
+        permanent: true,
+      },
+      {
+        source: "/forms/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.truequestlearning.com",
+          },
+        ],
+        destination: "https://forms.truequestlearning.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
