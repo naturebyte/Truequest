7ab459d13710f319bf38c81a48076d4e46abfeeb
M Nabeel <unfolks@gmail.com>
2026-04-07 23:34:54 +0530

    forms

---
+++ proxy.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const ROOT_DOMAINS = new Set(["truequestlearning.com", "www.truequestlearning.com"]);
+const FORMS_SUBDOMAIN = "forms.truequestlearning.com";
+
+export function proxy(request: NextRequest) {
+  const hostname = request.nextUrl.hostname.toLowerCase();
+  const pathname = request.nextUrl.pathname;
+
+  if (!ROOT_DOMAINS.has(hostname)) {
+    return NextResponse.next();
+  }
+
+  if (!pathname.startsWith("/forms")) {
+    return NextResponse.next();
+  }
+
+  const mappedPath = pathname.replace(/^\/forms/, "") || "/";
+  const redirectUrl = new URL(request.url);
+  redirectUrl.hostname = FORMS_SUBDOMAIN;
+  redirectUrl.pathname = mappedPath;
+
+  return NextResponse.redirect(redirectUrl, 308);
+}
+
+export const config = {
+  matcher: ["/forms", "/forms/:path*"],
+};
